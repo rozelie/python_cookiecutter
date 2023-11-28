c9b4fa4338c172dac83cd7450572401fcf45c081
rozelie <ryan.ozelie@gmail.com>
2023-11-28 12:04:18 -0600

    Update dependencies to latest version

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -1,6 +1,7 @@
+"""{{ cookiecutter.project_description }}"""
+
 def main() -> None:
     """Project entrypoint."""
-    print("Hello from {{cookiecutter.project_slug}}")
 
 
 if __name__ == "__main__":
